590384576ba9dc80c90af790ef09a3a098ca295c
Kaito0786 <allanfonseca786@gmail.com>
2024-10-30 16:49:23 +0530

    Logger and Exception handling added

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingExecption
+import sys
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+
+    try:
+        raise Exception("We are testing exception")
+    except Exception as e:
+        housing = HousingExecption(e,sys) 
+        logging.info(housing.error_message)
+        logging.info("We are testings logging")
     return 'Hello, World!'
 
 if __name__ == '__main__':
